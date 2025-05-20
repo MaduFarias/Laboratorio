7bfe51f2a850eb8a108f9a484c995e15e914f187
Karolline Lopes <karoll_lopes_ribeiro@hotmail.com>
2025-05-20 09:46:02 -0300

    feat: sugestões de melhorias

--- Semana03/Calculadora.java
+++ Semana03/Calculadora.java
@@ -6,6 +6,7 @@ public class Calculadora {
     static Scanner scanner = new Scanner(System.in);
 
     static void Calcular(String operation) {
+        //double result = 0;
         System.out.print("Digite o primeiro número: ");
         double firstNumber = scanner.nextDouble();
         scanner.nextLine();
@@ -18,6 +19,7 @@ public class Calculadora {
             case "adição":
             case "+":
                 double adicaoResultado = firstNumber + secondNumber;
+                //resultado = firstNumber + secondNumber;
                 System.out.println("Resultado: " + firstNumber + " + " + secondNumber + " = " + adicaoResultado);
                 break;
             case "subtração":
@@ -32,8 +34,9 @@ public class Calculadora {
                 break;
             case "divisão":
             case "/":
-                if (secondNumber == 0){
+                if (secondNumber == 0) {
                     System.out.println("Erro: Divisão por zero não é permitida. Encerrando...");
+                    //System.exit(0);
                 } else {
                     double divisaoResultado = firstNumber / secondNumber;
                     System.out.println("Resultado: " + firstNumber + " / " + secondNumber + " = " + divisaoResultado);
@@ -42,34 +45,50 @@ public class Calculadora {
             default:
                 break;
         }
+        //System.out.println("Resultado: " + firstNumber + " " + operation + " " + secondNumber + " = " + resultado);
     }
 
     public static void main(String[] args) {
-        while(true) {
+        //String[] allowedOperation = {"+", "-", "*", "/"};
+        //String[] allowedOperationDesc = {"adicao", "subtracao", "multiplicacao", "divisao"};
+
+        while (true) {
             System.out.println("\n== CALCULADORA BÁSICA ==");
             System.out.print("""
-                Digite uma operação!
-                adição          +
-                subtração       -
-                multiplicação   *
-                divisão         /
-                >>>\s""");
+                    Digite uma operação!
+                    adição          +
+                    subtração       -
+                    multiplicação   *
+                    divisão         /
+                    >>>\s""");
             String operation = scanner.nextLine().trim().toLowerCase();
 
+            //Comando para tirar acentuação
+            //(!Arrays.asList(allowedOperations).contains(operation)) || 
+            //(!Arrays.asList(allowedOperationsDesc).contains(operation))
 
-            if ((operation.equals("adição") ||  operation.equals("+") || operation.equals("subtração") || operation.equals("-") || operation.equals("multiplicação") || operation.equals("*") || operation.equals("divisão") || operation.equals("/"))) {
+            if ((operation.equals("adição") || operation.equals("+") ||
+                    operation.equals("subtração") || operation.equals("-") ||
+                    operation.equals("multiplicação") || operation.equals("*") ||
+                    operation.equals("divisão") || operation.equals("/"))) {
+
                 Calcular(operation);
+
             } else {
                 System.out.println("Operação inválida, reinicie o programa");
+                //System.exit(0);
             }
 
-
             System.out.print("Deseja fazer outra operação? (sim/não) ");
-            String resposta = scanner.nextLine();
-            if (!(resposta.equalsIgnoreCase("sim".trim()))) {
+            String resposta = scanner.nextLine().trim();
+
+            if (!(resposta.equalsIgnoreCase("sim"))) {
                 System.out.println("O programa será encerrado, obrigada por sua participação!");
                 break;
             }
         }
+            
+        scanner.close();
     }
+
 }
