f61a979ac0ade6f3d56ee088a2f1c300dffad3e5
Maria Eduarda Aragão Farias <madufarias47@gmail.com>
2025-06-12 02:23:51 -0400

    Rascunho Jogo e add menu

--- Semana05/QuemSouEu/src/main/java/org/example/Jogo.java
+++ Semana05/QuemSouEu/src/main/java/org/example/Jogo.java
@@ -1,9 +1,17 @@
 package org.example;
 
+import org.json.JSONArray;
+
+import java.util.Random;
+
 public class Jogo {
     String categoria;
     String dica;
     String respostas;
+    int elementosTamanho;
+    JSONArray elementos;
+
+    static Random random = new Random();
 
     void Sortear() {
 
